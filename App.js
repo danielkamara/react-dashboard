208b40dfb490bc8e2053d3a43a5216ee44ffa92f
daniel <danlikestech@gmail.com>
2022-02-06 23:19:00 -0600

    added image

--- App.js
+++ App.js
@@ -1,4 +1,74 @@
-ReactDOM.render(
-  <h1>Commence Dashboard Creation!</h1>,
-  document.querySelector("#main")
-);
+class App extends React.Component {
+  render() {
+    return (
+      <div class="container">
+        <Dashboard />,
+        <Reviews />,
+        <WebsiteVisitors />,
+        <AverageRating />,
+        <SentimentAnalysis />
+      </div>
+    );
+  }
+}
+class Dashboard extends React.Component {
+  render() {
+    return (
+      <div class="dashboard">
+        <h3>Dashboard</h3>
+        <li>
+          <ul>Widgets</ul>
+          <ul>Reviews</ul>
+          <ul>Customers</ul>
+          <ul>Online Analysis</ul>
+          <ul>Settings</ul>
+        </li>
+      </div>
+    );
+  }
+}
+class Reviews extends React.Component {
+  render() {
+    return (
+      <div class="reviews">
+        <h3>Reviews</h3>
+        <p>1,281</p>
+      </div>
+    );
+  }
+}
+class WebsiteVisitors extends React.Component {
+  render() {
+    return (
+      <div class="visitors">
+        <h3>Website Visitors</h3>
+        <p>821</p>
+        <div class="inner-box"></div>
+      </div>
+    );
+  }
+}
+class AverageRating extends React.Component {
+  render() {
+    return (
+      <div class="rating">
+        <h3>AverageRating</h3>
+        <p>4.6</p>
+      </div>
+    );
+  }
+}
+class SentimentAnalysis extends React.Component {
+  render() {
+    return (
+      <div class="analysis">
+        <h3>SentimentAnalysis</h3>
+        <p>960</p>
+        <p>122</p>
+        <p>321</p>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector("#main"));
